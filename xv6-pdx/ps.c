739faf24ff420d538d3471c448237c321eeaa622
houwdai <CindiTriFitikasari_1313618025@mhs.unj.ac.id>
2021-05-26 23:04:26 +0700

    update ps, usertest ss

--- xv6-pdx/ps.c
+++ xv6-pdx/ps.c
@@ -9,33 +9,24 @@ main(void)
 {
   struct uproc* table;
   int i;
-  uint max = 72;
+  uint max = 1; // 1, 16, 64, 72
   int catch = 0;
-  uint elapsed, decimal, seconds, seconds_decimal;
+  uint elapsed_sec, elapsed_mod, total_sec, total_mod;
   table = malloc(sizeof(struct uproc) * max);
   catch = getprocs(max, table);
   if(catch == -1)
     printf(1, "\nError: Invalid max or NULL uproc table\n");
   else {
+    printf(1, "MAX =%d\t", max);
     printf(1, "\nPID\tName\tUID\tGID\tPPID\tElapsed\tCPU\tState\tSize");
     for(i = 0;i < catch;++i) {
-      decimal = table[i].elapsed_ticks % 1000;
-      elapsed = table[i].elapsed_ticks / 1000;
-      seconds_decimal = table[i].CPU_total_ticks % 1000;
-      seconds = table[i].CPU_total_ticks / 1000;
-      printf(1, "\n%d\t%s\t%d\t%d\t%d\t%d.", table[i].pid, table[i].name, table[i].uid, table[i].gid, table[i].ppid, elapsed);
-      if(decimal < 10)
-        printf(1, "00");
-      else if(decimal < 100)
-        printf(1, "0");
-      printf(1, "%d\t%d.", decimal, seconds);
-      if(seconds_decimal < 10)
-        printf(1, "00");
-      else if(seconds_decimal < 100)
-        printf(1, "0");
-      printf(1, "%d\t%s\t%d", seconds_decimal, table[i].state, table[i].size);
+      elapsed_mod = table[i].elapsed_ticks % 1000;
+      elapsed_sec = table[i].elapsed_ticks / 1000;
+      total_mod = table[i].CPU_total_ticks % 1000;
+      total_sec = table[i].CPU_total_ticks / 1000;
+      printf(1, "\n%d\t%s\t%d\t%d\t%d\t%d.%d\t%d.%d\t", table[i].pid, table[i].name, table[i].uid, table[i].gid, table[i].ppid, elapsed_sec, elapsed_mod, total_sec, total_mod);
+      printf(1, "%s\t%d\n", table[i].state, table[i].size);
     }
-    printf(1, "\n");
   }
   free(table);
   exit();
